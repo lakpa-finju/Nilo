a839e310b5ef9423288e76c14f4ad5e98bb5ae58
lakpa-finju <connectedtofinju1@gmail.com>
2023-04-16 14:41:48 -0400

    Xcode 12 branch

--- firstProject/ContentView.swift
+++ firstProject/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
             VStack {
                 if isTextShowing {
-                    Text("Hello, world!")
+                    Text("Hello, world! XCode12 Branch ")
                         .padding()
                         .font(.title)
                 } else {
