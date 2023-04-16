b4de2acdb462f8cb87de65a2b470234298cc8425
lakpa-finju <connectedtofinju1@gmail.com>
2023-04-16 13:53:42 -0400

    Initial Commit

---
+++ firstProject/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  firstProject
+//
+//  Created by lakpafinju sherpa on 2023-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isTextShowing = true
+    var body: some View {
+            VStack {
+                if isTextShowing {
+                    Text("Hello, world!")
+                        .padding()
+                } else {
+                    /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
+                        .padding()
+                }
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    isTextShowing.toggle()
+                }
+            }
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstProject/firstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstProjectApp.swift
+//  firstProject
+//
+//  Created by lakpafinju sherpa on 2023-04-16.
+//
+
+import SwiftUI
+
+@main
+struct firstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
