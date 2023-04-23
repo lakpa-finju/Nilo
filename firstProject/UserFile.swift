ea2a8660eb73a698edc7bb1cddf1141119369d96
lakpa-finju <connectedtofinju1@gmail.com>
2023-04-22 22:59:34 -0400

    created userFile model to handle image upload

---
+++ firstProject/UserFile.swift
@@ -0,0 +1,15 @@
+//
+//  UserFile.swift
+//  firstProject
+//
+//  Created by lakpafinju sherpa on 2023-04-22.
+//
+
+import Foundation
+//The file type is image/jpeg
+struct UserFile: Identifiable{
+    var id: String
+    var name: String
+    var caption: String
+    
+}
